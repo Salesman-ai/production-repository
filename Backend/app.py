725c4b0ed854248f2c196a0f8459ca84e217fec1
GitHub Action <action@github.com>
2023-03-27 23:15:44 +0000

    Update Backend folder [via Backend repository]

--- Backend/app.py
+++ Backend/app.py
@@ -25,33 +25,35 @@ def summary(body, status_code):
                               mimetype='application/json')
 
 
-@app.route('/api/get-price')
+@app.route('/api/get-price', methods = ['GET', 'POST'])
 def get_price():
-    #prediction = None
-    response = None
-    #code = None
-    log.backend.info(f"Function 'get_price()' started")
-    log.request.info(f"Request was received from <{request.remote_addr}>.")
-    try:
-        request_data = request.args
-        log.request.info(f"Request was sent to the prediction module")
+    if request.method == 'POST':
+        #prediction = None
+        test = request.form
+        print(test)
+        response = None
+        #code = None
+        log.backend.info(f"Function 'get_price()' started")
+        log.request.info(f"Request was received from <{request.remote_addr}>.")
         try:
-            #prediction = requests.get(url = os.getenv('PREDICTION_URL'), params = request_data)
-            #prediction = request_data['kurwa']
-            log.request.info(f"Response was received from  prediction module")
+            log.request.info(f"Request was sent to the prediction module")
+            try:
+                #prediction = requests.get(url = os.getenv('PREDICTION_URL'), params = request_data)
+                #prediction = request_data['kurwa']
+                log.request.info(f"Response was received from  prediction module")
+            
+            except Exception as error:
+                log.request.error(f"Response was not obtained from the prediction model. Error log - {error}")
         
         except Exception as error:
-            log.request.error(f"Response was not obtained from the prediction model. Error log - {error}")
-    
-    except Exception as error:
-        log.backend.error(f"Function 'get_price()' failed. Status: {error}")
-        log.request.error(f"Request received from <{request.remote_addr}> failed.")
-    finally:
-        #response = summary(10000, 200)
-        #insert_price(1, str(request.remote_addr), "honda", "civic", 10000)
-        log.request.info(f"Response returned")
-        log.backend.info(f"Function 'get_price()' finished")
-        return response
+            log.backend.error(f"Function 'get_price()' failed. Status: {error}")
+            log.request.error(f"Request received from <{request.remote_addr}> failed.")
+        finally:
+            response = summary(10000, 200)
+            #insert_price(1, str(request.remote_addr), "honda", "civic", 10000)
+            log.request.info(f"Response returned")
+            log.backend.info(f"Function 'get_price()' finished")
+            return response
 
 
 if __name__ == '__main__':
