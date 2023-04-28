5d4e0b8f76664df1046cb8e1cd2698f46a09e669
GitHub Action <action@github.com>
2023-04-28 06:26:29 +0000

    Update Backend folder [via Backend repository]

--- Backend/database/db_conn.py
+++ Backend/database/db_conn.py
@@ -11,7 +11,7 @@ config_path = Path(os.path.abspath(__file__)).parent / '../config.cfg'
 load_dotenv(dotenv_path=config_path)
 
 def generate_connection(database_name=os.environ.get("POSTGRES_DB")):
-    time.sleep(60)
+    time.sleep(15)
     try:
         conn = psycopg2.connect(f"postgresql://{os.environ.get('POSTGRES_USER')}:{os.environ.get('POSTGRES_PASS')}@{os.environ.get('POSTGRES_HOST')}/{os.environ.get('POSTGRES_DB')}")
         return conn
