c315bfe5c18d02b352d78b773a8c62e50968515c
GitHub Action <action@github.com>
2023-05-24 21:24:44 +0000

    Update Backend folder [via Backend repository]

--- Backend/test_app.py
+++ Backend/test_app.py
@@ -3,6 +3,14 @@ import unittest
 import requests
 from database.db_query import check_connection
 import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+try:
+    config_path = Path('./config.cfg')
+    load_dotenv(dotenv_path=config_path)
+except Exception as error:
+    exit()
 
 class RunTest(unittest.TestCase):
 
@@ -18,7 +26,7 @@ class RunTest(unittest.TestCase):
             "engineDisplacement": 1.2,
             "power": 120,
         }
-        result = requests.get("http://127.0.0.1:8090/api-prediction/get-predict", params=parameters)
+        result = requests.get(os.environ.get("PREDICTION_URL") +  ":" + os.environ.get("PREDICTION_PORT") + "/api-prediction/get-predict", params=parameters)
         txt = result.text.replace('"','')
         self.assertEqual(result.status_code, 200)
         self.assertIsInstance(float(txt), float)
@@ -32,7 +40,7 @@ class RunTest(unittest.TestCase):
             "engineDisplacement": 1.2,
             "power": 120,
         }
-        result = requests.get("http://127.0.0.1:8090/api-prediction/get-predict", params=parameters)
+        result = requests.get(os.environ.get("PREDICTION_URL") +  ":" + os.environ.get("PREDICTION_PORT") + "/api-prediction/get-predict", params=parameters)
         self.assertEqual(result.status_code, 414)
 
     def test_if_year_value_are_incorrect_low(self):
@@ -47,7 +55,7 @@ class RunTest(unittest.TestCase):
             "engineDisplacement": 1.2,
             "power": 120,
         }
-        result = requests.get("http://127.0.0.1:8090/api-prediction/get-predict", params=parameters)
+        result = requests.get(os.environ.get("PREDICTION_URL") +  ":" + os.environ.get("PREDICTION_PORT") + "/api-prediction/get-predict", params=parameters)
         self.assertEqual(result.status_code, 415)
 
     def test_if_year_value_are_incorrect_high(self):
@@ -62,7 +70,7 @@ class RunTest(unittest.TestCase):
             "engineDisplacement": 1.2,
             "power": 120,
         }
-        result = requests.get("http://127.0.0.1:8090/api-prediction/get-predict", params=parameters)
+        result = requests.get(os.environ.get("PREDICTION_URL") +  ":" + os.environ.get("PREDICTION_PORT") + "/api-prediction/get-predict", params=parameters)
         self.assertEqual(result.status_code, 416)
 
     def test_if_engine_value_are_incorrect(self):
@@ -77,7 +85,7 @@ class RunTest(unittest.TestCase):
             "engineDisplacement": 30,
             "power": 120,
         }
-        result = requests.get("http://127.0.0.1:8090/api-prediction/get-predict", params=parameters)
+        result = requests.get(os.environ.get("PREDICTION_URL") +  ":" + os.environ.get("PREDICTION_PORT") + "/api-prediction/get-predict", params=parameters)
         self.assertEqual(result.status_code, 418)
 
     def test_if_power_value_are_incorrect(self):
@@ -92,20 +100,20 @@ class RunTest(unittest.TestCase):
             "engineDisplacement": 1.2,
             "power": 1200,
         }
-        result = requests.get("http://127.0.0.1:8090/api-prediction/get-predict", params=parameters)
+        result = requests.get(os.environ.get("PREDICTION_URL") +  ":" + os.environ.get("PREDICTION_PORT") + "/api-prediction/get-predict", params=parameters)
         self.assertEqual(result.status_code, 417)
 
     def test_if_params_are_not_sended(self):
-        result = requests.get("http://127.0.0.1:8090/api-prediction/get-predict")
+        result = requests.get(os.environ.get("PREDICTION_URL") +  ":" + os.environ.get("PREDICTION_PORT") + "/api-prediction/get-predict")
         self.assertEqual(result.status_code, 420)
 
-    # def test_check_database_connection(self):
-    #     result = check_connection()
-    #     self.assertTrue(result)
+    def test_check_database_connection(self):
+        result = check_connection()
+        self.assertTrue(result)
 
-    # def test_is_config_file_exists(self):
-    #     result = os.path.isfile('./config.cfg')
-    #     self.assertTrue(result)
+    def test_is_config_file_exists(self):
+        result = os.path.isfile('./config.cfg')
+        self.assertTrue(result)
     
 if __name__ == '__main__':
     unittest.main()
